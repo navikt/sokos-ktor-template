bdc7e4ddadff6a3ca0eb0d4a35124fe4f75acd21
mudasar187 <mudasar_cheema@hotmail.com>
2022-06-30 16:40:28 +0200

    fikse routing i samme slengen

--- src/main/kotlin/no/nav/sokos/prosjektnavn/api/HelloApi.kt
+++ src/main/kotlin/no/nav/sokos/prosjektnavn/api/HelloApi.kt
@@ -1,16 +1,13 @@
 package no.nav.sokos.prosjektnavn.api
 
-import io.ktor.http.ContentType
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
-import io.prometheus.client.exporter.common.TextFormat
-import no.nav.sokos.prosjektnavn.metrics.Metrics
 
-const val BASE_ENDPOINT = "/okonomi-ktor-template"
+const val BASE_ENDPOINT = "okonomi-ktor-template"
 
 fun Application.helloApi() {
     routing {
