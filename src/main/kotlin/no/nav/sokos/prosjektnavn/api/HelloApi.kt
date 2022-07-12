dea5d2d7caedd4a60add97ce54b13d05fe8a78ff
mudasar187 <mudasar_cheema@hotmail.com>
2022-07-12 20:51:21 +0200

    teste alerterator

--- src/main/kotlin/no/nav/sokos/prosjektnavn/api/HelloApi.kt
+++ src/main/kotlin/no/nav/sokos/prosjektnavn/api/HelloApi.kt
@@ -1,7 +1,9 @@
 package no.nav.sokos.prosjektnavn.api
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
+import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.route
@@ -12,8 +14,14 @@ const val BASE_ENDPOINT = "okonomi-ktor-template"
 fun Application.helloApi() {
     routing {
         route(BASE_ENDPOINT) {
-            get("/hello") {
-                call.respondText { "Hello World from NAIS!" }
+            get("/forbidden") {
+                call.respond(HttpStatusCode.Forbidden)
+            }
+            get("/unauthorized") {
+                call.respond(HttpStatusCode.Unauthorized)
+            }
+            get("/internalservererror") {
+                call.respond(HttpStatusCode.InternalServerError)
             }
         }
     }
