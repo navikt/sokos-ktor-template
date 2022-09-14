9fd62f568f352372186245dc108060fca2294676
Mudasar Cheema <mudasar_cheema@hotmail.com>
2022-09-14 12:38:21 +0200

    Update setupTemplate.sh

--- setupTemplate.sh
+++ setupTemplate.sh
@@ -9,6 +9,6 @@ read -p 'Project name (sokos-xxxx): ' projectName
 read -p 'Artifact name (xxxx): ' artifactName
 echo
 
-grep -rl $default --exclude=setupTemplate.sh | xargs -I@ sed -i '' "s|$default|$projectName|g" @
+grep -rl $default --exclude=setupTemplate.sh | xargs -I@ sed -i '' "s|$default|sokos-$projectName|g" @
 grep -rl $defaultArtifactName --exclude=setupTemplate.sh  | xargs -I@ sed -i '' "s|$defaultArtifactName|$artifactName|g" @
 mv src/main/kotlin/no/nav/sokos/prosjektnavn "src/main/kotlin/no/nav/sokos/$artifactName"
