3fae9a6e2fdd69eeda0fa81c4ad6d636b5ee1cfb
mudasar187 <mudasar_cheema@hotmail.com>
2022-09-27 15:34:34 +0200

    oppdaterer libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,12 +9,12 @@ val logbackVersion = "1.4.1"
 val logstashVersion = "7.2"
 val jacksonVersion = "2.13.4"
 val prometheusVersion = "1.9.4"
-val kotlinLoggingVersion = "2.1.23"
+val kotlinLoggingVersion = "3.0.0"
 
 plugins {
     kotlin("jvm") version "1.7.0"
     kotlin("plugin.serialization") version "1.7.0"
-    id("org.openapi.generator") version "6.1.0"
+    id("org.openapi.generator") version "6.2.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
