ed93e1426225d46d1b039d5ec93fe40c4365e886
mudasar187 <mudasar_cheema@hotmail.com>
2023-03-23 20:51:26 +0100

    trigge deploy

--- src/main/kotlin/no/nav/sokos/prosjektnavn/service/DummyService.kt
+++ src/main/kotlin/no/nav/sokos/prosjektnavn/service/DummyService.kt
@@ -4,6 +4,6 @@ import no.nav.sokos.prosjektnavn.domain.DummyDomain
 
 class DummyService {
     fun sayHello(): DummyDomain {
-        return DummyDomain("Hello from Dummy Api, this is a demo!")
+        return DummyDomain("Hello World!")
     }
 }
