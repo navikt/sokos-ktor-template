d5a676c0a4ac8373d0c7ae2671e6eb62f0813032
mudasar187 <mudasar_cheema@hotmail.com>
2023-04-01 21:51:58 +0200

    teste ut deploy til gar for å se at riktig image kommer i GAR

--- src/main/kotlin/no/nav/sokos/prosjektnavn/service/DummyService.kt
+++ src/main/kotlin/no/nav/sokos/prosjektnavn/service/DummyService.kt
@@ -4,6 +4,6 @@ import no.nav.sokos.prosjektnavn.domain.DummyDomain
 
 class DummyService {
     fun sayHello(): DummyDomain {
-        return DummyDomain("Gi meg siste image :(")
+        return DummyDomain("Hello World! Greeting sokos-ktor-template!")
     }
 }
