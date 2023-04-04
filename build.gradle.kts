1cb515e9c3241b3366f102786b6f51f968ef2f87
mudasar187 <mudasar_cheema@hotmail.com>
2023-04-04 13:23:56 +0200

    oppgradere libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 plugins {
-    kotlin("jvm") version "1.8.10"
-    kotlin("plugin.serialization") version "1.8.10"
-    id("org.openapi.generator") version "6.4.0"
+    kotlin("jvm") version "1.8.20"
+    kotlin("plugin.serialization") version "1.8.20"
+    id("org.openapi.generator") version "6.5.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 
     application
