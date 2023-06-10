9ff1000ddd975e43a5e2147b2d04b7cd41e21dec
mudasar187 <mudasar_cheema@hotmail.com>
2023-06-10 20:47:33 +0200

    tester alarmer meldinger om det er riktig formatering

--- src/main/kotlin/no/nav/sokos/prosjektnavn/api/DummyApi.kt
+++ src/main/kotlin/no/nav/sokos/prosjektnavn/api/DummyApi.kt
@@ -6,10 +6,12 @@ import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import io.ktor.server.routing.route
+import mu.KotlinLogging
 import no.nav.sokos.prosjektnavn.config.AUTHENTICATION_NAME
 import no.nav.sokos.prosjektnavn.config.authenticate
 import no.nav.sokos.prosjektnavn.service.DummyService
 
+private val log = KotlinLogging.logger {}
 fun Route.dummyApi(
     dummyService: DummyService,
     useAuthentication: Boolean
@@ -22,6 +24,12 @@ fun Route.dummyApi(
             }
             get("goodbye") {
                 call.respond(HttpStatusCode.BadRequest)
+            }
+            get("error") {
+                log.error { "Noe gikk galt" }
+            }
+            get("warning") {
+                log.warn { "Noe gikk galt" }
             }
         }
     }
