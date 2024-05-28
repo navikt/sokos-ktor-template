abf224743f46c2c83c033eea16298f6d6544986a
mudasar187 <mudasar_cheema@hotmail.com>
2024-05-28 15:36:29 +0200

    oppdaterer applicationstate til å bruke event for å sjekke alive og ready

--- src/main/kotlin/no/nav/sokos/prosjektnavn/Application.kt
+++ src/main/kotlin/no/nav/sokos/prosjektnavn/Application.kt
@@ -6,20 +6,16 @@ import io.ktor.server.engine.stop
 import io.ktor.server.netty.Netty
 import no.nav.sokos.prosjektnavn.config.PropertiesConfig
 import no.nav.sokos.prosjektnavn.config.commonConfig
+import no.nav.sokos.prosjektnavn.config.configureLifecycleConfig
 import no.nav.sokos.prosjektnavn.config.configureSecurity
 import no.nav.sokos.prosjektnavn.config.routingConfig
 import java.util.concurrent.TimeUnit
 
 fun main() {
-    val applicationState = ApplicationState()
-    val applicationConfiguration = PropertiesConfig.Configuration()
-
-    HttpServer(applicationState, applicationConfiguration).start()
+    HttpServer().start()
 }
 
 class HttpServer(
-    private val applicationState: ApplicationState,
-    private val applicationConfiguration: PropertiesConfig.Configuration,
     port: Int = 8080,
 ) {
     init {
@@ -32,31 +28,29 @@ class HttpServer(
 
     private val embeddedServer =
         embeddedServer(Netty, port, module = {
-            serverModule(applicationConfiguration, applicationState)
-            applicationState.initialized = true
+            serverModule()
         })
 
     fun start() {
-        applicationState.running = true
         embeddedServer.start(wait = true)
     }
 
     private fun stop() {
-        applicationState.running = false
         embeddedServer.stop(5, 5, TimeUnit.SECONDS)
     }
 }
 
 class ApplicationState(
-    var running: Boolean = false,
-    var initialized: Boolean = false,
+    var ready: Boolean = true,
+    var alive: Boolean = true,
 )
 
-fun Application.serverModule(
-    applicationConfiguration: PropertiesConfig.Configuration,
-    applicationState: ApplicationState,
-) {
+fun Application.serverModule() {
+    val applicationState = ApplicationState()
+    val applicationConfiguration = PropertiesConfig.Configuration()
+
     commonConfig()
+    configureLifecycleConfig(applicationState)
     configureSecurity(applicationConfiguration.azureAdProperties, applicationConfiguration.useAuthentication)
     routingConfig(applicationState, applicationConfiguration.useAuthentication)
 }
