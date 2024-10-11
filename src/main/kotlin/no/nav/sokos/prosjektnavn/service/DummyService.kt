53d72ba03eb5d072515596b0596442fcf10d3947
mudasar187 <mudasar_cheema@hotmail.com>
2024-10-11 22:06:14 +0200

    tester adde label

--- src/main/kotlin/no/nav/sokos/prosjektnavn/service/DummyService.kt
+++ src/main/kotlin/no/nav/sokos/prosjektnavn/service/DummyService.kt
@@ -6,6 +6,6 @@ import no.nav.sokos.prosjektnavn.metrics.Metrics
 class DummyService {
     fun sayHello(): DummyDomain {
         Metrics.exampleCounter.inc()
-        return DummyDomain("Hello Team Moby!")
+        return DummyDomain("Hello Team Moby! This is a dummy response.")
     }
 }
