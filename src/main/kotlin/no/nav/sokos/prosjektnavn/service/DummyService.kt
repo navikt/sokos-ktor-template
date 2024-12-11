17b2e02f9f53073a2c34a5764821dc1949a883d4
mudasar187 <mudasar_cheema@hotmail.com>
2024-12-11 19:32:13 +0100

    trigge deploy for å teste workflows

--- src/main/kotlin/no/nav/sokos/prosjektnavn/service/DummyService.kt
+++ src/main/kotlin/no/nav/sokos/prosjektnavn/service/DummyService.kt
@@ -6,6 +6,6 @@ import no.nav.sokos.prosjektnavn.metrics.Metrics
 class DummyService {
     fun sayHello(): DummyDomain {
         Metrics.exampleCounter.inc()
-        return DummyDomain("This is a template for Team Moby")
+        return DummyDomain("This is a template for Team Monster")
     }
 }
